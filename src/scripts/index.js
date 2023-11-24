eccc8b3f5666f1b889a6dfefdf3ffbc28acf55d3
Srefaldi <2110131110004@mhs.ulm.ac.id>
2023-11-25 04:05:31 +0800

    update home and add code frontend

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,6 @@
 import 'regenerator-runtime';
-
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/appbar/appbar.css';
 import '../styles/responsive/responsive.css';
 import '../styles/main.css';

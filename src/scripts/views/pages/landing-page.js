a4082a726cb75fa0c26a5cc0c810199b532efe6d
Srefaldi <2110131110004@mhs.ulm.ac.id>
2023-11-26 12:33:10 +0800

    fixed landing page style

--- src/scripts/views/pages/landing-page.js
+++ src/scripts/views/pages/landing-page.js
@@ -1,7 +1,37 @@
 const LandingPage = {
+  imagePaths: [
+    '/images/home/rishabh-pandoh-klpWbwujpUg-unsplash.jpg'
+  ],
   async render() {
     return `
-            <h2>Landing Page</h2>
+    <!-- Hero Section -->
+    <div class="container-fluid px-0 overflow-hidden"> 
+      <div class="row mx-0"> 
+        <div class="col-lg-12 px-0"> 
+          <img id="homeImage" class="img-fluid w-100" src="${this.imagePaths[0]}" alt="Foto Home">
+        </div>
+      </div>
+    </div>
+  <div class="containerr mb-5">
+    <div class="container__leftt">
+      <div class="left__contentt">
+      </div>
+    </div>
+    <div class="container__rightt">
+      <div class="right__contentt">
+        <h1>EduFauna</h1>
+        <h4>Aplikasi Edukasi Fauna Indonesia</h4>
+        <p>
+          Selamat datang di Aplikasi Edukasi Fauna Indonesia! Temukan keindahan dan keragaman satwa Indonesia melalui perjalanan edukatif. 
+          Mari bersama-sama menjelajahi keajaiban alam dan memahami kehidupan fauna Indonesia yang menakjubkan.
+        </p>
+        <div class="action__btnss">
+          <button class="btn primary__btnn">Explore</button>
+          <button class="btn secondary__btnn">See More</button>
+        </div>
+      </div> 
+    </div>
+  </div>
   
           `;
   },

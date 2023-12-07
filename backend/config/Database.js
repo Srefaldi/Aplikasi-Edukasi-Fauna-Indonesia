b888ca16704a1159ee9184c1ff5258fe5898841b
KhoyrurRoykhan <khoirurroykan007@gmail.com>
2023-12-08 00:41:40 +0800

    add reviewer controller (CRUD)

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const Database = new Sequelize('db_apl_edu_fauna_indonesia','root','',{
+const Database = new Sequelize('db_apl_edu_fauna_indonesia','root','123',{
     host: "localhost",
     dialect: "mysql"
 });
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,6 +4,7 @@ import { verifyToken } from "../middleware/VerifyToken.js";
 import { refreshToken } from "../controllers/RefreshToken.js";
 import { AddFauna, deleteFaunaById, editFaunaById, getAllfauna, getFaunaById } from "../controllers/FaunaContents.js";
 import { AddQuiz } from "../controllers/QuizController.js";
+import { addReview, deleteReviewById, getAllReviews, getReviewById, updateReviewById } from "../controllers/Reviewer.js";
  
 const router = express.Router();
  
@@ -13,13 +14,22 @@ router.post('/login', Login);
 router.get('/token', refreshToken);
 router.delete('/logout', Logout);
 
-
+// Fauna
 router.post('/add-fauna', AddFauna);
 router.get('/get-allfauna', getAllfauna);
 router.get('/get-fauna/:faunaId', getFaunaById);
 router.put('/edit-fauna/:faunaId', editFaunaById);
 router.delete('/delete-fauna/:faunaId', deleteFaunaById);
 
+// Quiz
 router.post('/add-quiz', AddQuiz);
+
+// Reviewer
+router.post('/add-review', addReview);
+router.get('/get-allreview', getAllReviews);
+router.get('/get-review/:reviewId', getReviewById);
+router.put('/edit-review/:reviewId', updateReviewById);
+router.delete('/delete-review/:reviewId', deleteReviewById);
+
  
 export default router;

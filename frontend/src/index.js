d897286b6d81ac02984a859f2068fe681ef474da
Srefaldi <2110131110004@mhs.ulm.ac.id>
2023-12-15 17:18:28 +0800

    update pwa add install app in offline mode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import "bulma/css/bulma.css";
-import axios from "axios";
+import 'bulma/css/bulma.css';
+import axios from 'axios';
 import swDev from './swDev';
+
 axios.defaults.withCredentials = true;
 
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', () => {
-//     navigator.serviceWorker.register(new URL('./sw.js', import.meta.url))
-//       .then(registration => {
-//         console.log('Service Worker registered:', registration);
-//       })
-//       .catch(error => {
-//         console.error('Service Worker registration failed:', error);
-//       });
-//   });
-// }
+swDev(); 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -24,4 +15,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-swDev();
